dd865626f4bb8e40a7cdace07441ec59bd2660b3
dsbrooks <david.brooks85@gmail.com>
2013-10-02 14:55:05 +0900

    Fixed styles
    + updated config.pl to fix stylesheet linking

--- config.pl
+++ config.pl
@@ -19,7 +19,7 @@
 #use constant THREADS_LISTED => 40;				# Number of threads in the thread list
 #use constant REPLIES_PER_THREAD => 10;			# Replies shown
 #use constant S_ANONAME => 'Anonymous';			# Defines what to print if there is no text entered in the name field
-use constant DEFAULT_STYLE => 'board.css';		# Default CSS style title
+use constant DEFAULT_STYLE => 'css/board.css';		# Default CSS style title
 use constant FAVICON => '/favicon.ico';			# Path to the favicon for the board
 
 # Limitations
